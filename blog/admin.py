1b1227466940c4545c2aabeacef94651c4f95082
zhango <zhangsj0209@gmail.com>
2020-10-10 16:55:54 +0700

    changed the html for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+
+#admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['author','title']
